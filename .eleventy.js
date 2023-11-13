3b8cd013ff0b947d575b2f9514743ecb0de668c1
Wladislav ヴラド Artsimovich <wladislav.artsimovich@dmgmori.co.jp>
2023-11-13 13:14:33 +0900

    CSS minification

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
+const CleanCSS = require("clean-css");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
+	/* Syntax Highlighting */
 	eleventyConfig.addPlugin(syntaxHighlight);
+	/* The required CSS for the PrimJS color theme */
 	eleventyConfig.addPassthroughCopy("style/prism-gruvbox-dark.css");
+
+	/* CSS minifier as per https://www.11ty.dev/docs/quicktips/inline-css/ */
+	eleventyConfig.addFilter("cssmin", function (code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
 };
