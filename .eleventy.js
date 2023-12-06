b4ba20794da9d7e1a34ce13de86374a535ebbeba
Wladislav ヴラド Artsimovich <wladislav.artsimovich@dmgmori.co.jp>
2023-12-06 14:42:11 +0900

    Thumbnail handling

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
-const { execSync } = require('child_process');
 const CleanCSS = require("clean-css");
+const Image = require("@11ty/eleventy-img");
+const { execSync } = require('child_process');
 const faviconPlugin = require("eleventy-favicon");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
@@ -36,5 +37,24 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addShortcode("rawFile", (filename) => {
 		const bytesOfFile = fs.readFileSync(filename);
 		return bytesOfFile.toString();
+	});
+
+	/* Thumbnail maker */
+	eleventyConfig.addCollection("thumbnail", async function (collectionApi) {
+		// Get all posts
+		const posts = collectionApi.getFilteredByTag("post");
+
+		for (const post of posts) {
+			if (post.data.image) {
+				const image = await Image('./posts/' + post.url + '/' + post.data.image, {
+					widths: [150],
+					formats: ['jpeg'],
+					outputDir: './_site/' + post.url
+				});
+				post.data.image = post.url + '/' + image.jpeg[0].filename;
+			}
+		}
+
+		return posts;
 	});
 };
