6458fcbab4b25652ce55aab5f9675cd83f961d03
Wladislav ヴラド Artsimovich <wladislav.artsimovich@dmgmori.co.jp>
2023-12-15 17:42:10 +0900

    Split Javascript and shaders

--- posts/GLSL-noise-and-radial-gradient/GLSL-noise-and-radial-gradient.md
+++ posts/GLSL-noise-and-radial-gradient/GLSL-noise-and-radial-gradient.md
@@ -10,22 +10,28 @@ publicTags:
   - GameDev
 image: threshold.png
 ---
-The point of the Shader is to get banding free gradients, using a single pass and without sampling or texture taps to achieve banding free-ness. I'm using this for backgrounds with smooth gradients when doing graphics programming. It involves the best noise one-liner I have ever seen. That genius one-liner is not from me, but from  [Jorge Jimenez's presentation on how Gradient noise was implemented in Call of Duty Advanced Warfare](http://www.iryoku.com/next-generation-post-processing-in-call-of-duty-advanced-warfare). You can read it on the presentation's slide 123 onwards. It's described as:
+
+The point of the Shader is to get banding free gradients, using a single pass and without sampling or texture taps to achieve banding free-ness. I'm using this for backgrounds with smooth gradients when doing graphics programming. It involves the best noise one-liner I have ever seen. That genius one-liner is not from me, but from [Jorge Jimenez's presentation on how Gradient noise was implemented in Call of Duty Advanced Warfare](http://www.iryoku.com/next-generation-post-processing-in-call-of-duty-advanced-warfare). You can read it on the presentation's slide 123 onwards. It's described as:
+
 > [...] a noise function that we could classify as being half way between dithered and random, and that we called Interleaved Gradient Noise.
 
-| Threshold of gradient (zoomed) | Raw Noise (1:1 pixel size) |
-| ----------------- | ------------------- |
-|[![image](threshold.png)](threshold.png)|[![image](raw_noise.png)](raw_noise.png)|
+| Threshold of gradient (zoomed)           | Raw Noise (1:1 pixel size)               |
+| ---------------------------------------- | ---------------------------------------- |
+| [![image](threshold.png)](threshold.png) | [![image](raw_noise.png)](raw_noise.png) |
 
 Resulting Gradient: (Click image to view in 1:1 pixel scaling to properly judge the banding-freeness)
 [![image](radial.png)](radial.png)
 Technically the proper way to achieve this is to perform [error diffusion dithering](https://en.wikipedia.org/wiki/Error_diffusion), since that would breakup just the quantized steps of the gradient, without touching the color between the steps. But other than [ordered dithering](https://en.wikipedia.org/wiki/Ordered_dithering), there is no GPU friendly way to do this and [ordered dithering](https://en.wikipedia.org/wiki/Ordered_dithering) doesn't look nice. Adding noise in the context of gradients works just fine though, even though it's not proper error diffusion. Simply applying noise with the strength of one 8-bit grayscale value `(1.0 / 255.0) * gradientNoise(gl_FragCoord.xy)` side-steps a bunch of issues and the code footprint is tiny to boot.
 
+<script  id="vertex_1" type="x-shader/x-vertex">{% rawFile "posts/GLSL-noise-and-radial-gradient/circle.vs" %}</script>
+<script  id="fragment_1" type="x-shader/x-fragment">{% rawFile "posts/GLSL-noise-and-radial-gradient/circle.fs" %}</script>
 <canvas id="canvas_1"></canvas>
 <script src="canvas_1.js"></script>
 
 ## Vertex Shader
+
 Here is the Bufferless variant, but can be rewritten to work with even the most basic OpenGL or WebGL standard.
+
 ```glsl
 #version 330
 out vec2 tex;
@@ -42,7 +48,9 @@ void main()
     gl_Position = vec4(pos[gl_VertexID], 0.0, 1.0);
 }
 ```
+
 ## Fragment Shader
+
 ```glsl
 #version 330
 in vec2 tex;
--- posts/GLSL-noise-and-radial-gradient/canvas_1.js
+++ posts/GLSL-noise-and-radial-gradient/canvas_1.js
@@ -1,78 +1,50 @@
-"use strict";
 /* Init */
 const canvas = document.getElementById("canvas_1");
 const gl = canvas.getContext('webgl', { preserveDrawingBuffer: false });
-gl.clearColor(0, 0, 0, 0);
+
+/* Shaders */
+const vertexShader = createAndCompileShader(gl.VERTEX_SHADER, "vertex_1");
+const fragmentShader = createAndCompileShader(gl.FRAGMENT_SHADER, "fragment_1");
+
+const shaderProgram = gl.createProgram();
+gl.attachShader(shaderProgram, vertexShader);
+gl.attachShader(shaderProgram, fragmentShader);
+gl.linkProgram(shaderProgram);
+gl.useProgram(shaderProgram);
+
+
+/* Vertex Buffer */
+const unitQuad = new Float32Array([
+	-1.0, 1.0,
+	1.0, 1.0,
+	1.0, -1.0,
+	-1.0, -1.0
+]);
+
+const vertex_buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(unitQuad), gl.STATIC_DRAW);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+var vtx = gl.getAttribLocation(shaderProgram, "vtx");
+gl.enableVertexAttribArray(vtx);
+gl.vertexAttribPointer(vtx, 2, gl.FLOAT, false, 0, 0);
+
+const canvasToDisplaySizeMap = new Map([[canvas, [-1, -1]]]);
+const resizeObserver = new ResizeObserver(onResize);
+resizeObserver.observe(canvas, { box: 'content-box' });
 
 function redraw() {
 	gl.viewport(0, 0, canvas.width, canvas.height);
-	gl.clear(gl.COLOR_BUFFER_BIT);
-
-	const unitquadtex = new Float32Array([
-		-1.0, 1.0,
-		1.0, 1.0,
-		1.0, -1.0,
-		-1.0, -1.0
-	]);
-
-	const vertex_buffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(unitquadtex), gl.STATIC_DRAW);
-
-	// Create shaders
-	var vertCode =
-	'attribute vec2 vtx;' +
-	'varying vec2 uv;' +
-	'void main() {' +
-		'uv = vtx;' +
-		'gl_Position = vec4(vtx, 0.0, 1.0);' +
-	'}';
-	var vertShader = gl.createShader(gl.VERTEX_SHADER);
-	gl.shaderSource(vertShader, vertCode);
-	gl.compileShader(vertShader);
-	if (!gl.getShaderParameter(vertShader, gl.COMPILE_STATUS)) {
-		console.error('Vertex shader compilation error: ' + gl.getShaderInfoLog(vertShader));
-	}
-	var fragCode =
-		'precision mediump float;' +
-		'varying vec2 uv;' +
-		'void main(void) {' +
-		'     if(length(uv) < 1.0)' +
-		'         gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);' +
-		'     else' +
-		'         discard;' +
-		'}';
-
-	var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-	gl.shaderSource(fragShader, fragCode);
-	gl.compileShader(fragShader);
-
-	if (!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS)) {
-		console.error('Fragment shader compilation error: ' + gl.getShaderInfoLog(fragShader));
-	}
-
-	// Create shader program
-	var shaderProgram = gl.createProgram();
-	gl.attachShader(shaderProgram, vertShader);
-	gl.attachShader(shaderProgram, fragShader);
-	gl.linkProgram(shaderProgram);
-	gl.useProgram(shaderProgram);
-
-	// Bind vertex buffer object
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-
-	// Get the attribute location
-	var vtx = gl.getAttribLocation(shaderProgram, "vtx");
-	gl.enableVertexAttribArray(vtx);
-	gl.vertexAttribPointer(vtx, 2, gl.FLOAT, false,
-		2 * Float32Array.BYTES_PER_ELEMENT, 0);
-		
-	// Draw the triangle
 	gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
-	console.log("redrawn");
 }
 
-const canvasToDisplaySizeMap = new Map([[canvas, [-1, -1]]]);
+function createAndCompileShader(type, source) {
+	const shader = gl.createShader(type);
+	gl.shaderSource(shader, document.getElementById(source).text);
+	gl.compileShader(shader);
+	return shader;
+}
 
 function onResize(entries) {
 	for (const entry of entries) {
@@ -80,23 +52,18 @@ function onResize(entries) {
 		let height;
 		let dpr = window.devicePixelRatio;
 		if (entry.devicePixelContentBoxSize) {
-			// NOTE: Only this path gives the correct answer
-			// The other 2 paths are an imperfect fallback
-			// for browsers that don't provide anyway to do this
 			width = entry.devicePixelContentBoxSize[0].inlineSize;
 			height = entry.devicePixelContentBoxSize[0].blockSize;
-			dpr = 1; // it's already in width and height
+			dpr = 1;
 		} else if (entry.contentBoxSize) {
 			if (entry.contentBoxSize[0]) {
 				width = entry.contentBoxSize[0].inlineSize;
 				height = entry.contentBoxSize[0].blockSize;
 			} else {
-				// legacy
 				width = entry.contentBoxSize.inlineSize;
 				height = entry.contentBoxSize.blockSize;
 			}
 		} else {
-			// legacy
 			width = entry.contentRect.width;
 			height = entry.contentRect.height;
 		}
@@ -109,19 +76,13 @@ function onResize(entries) {
 	}
 }
 
-const resizeObserver = new ResizeObserver(onResize);
-resizeObserver.observe(canvas, { box: 'content-box' });
-
 function resizeCanvasToDisplaySize(canvas) {
-	// Get the size the browser is displaying the canvas in device pixels.
 	const [displayWidth, displayHeight] = canvasToDisplaySizeMap.get(canvas);
 
-	// Check if the canvas is not the same size.
 	const needResize = canvas.width !== displayWidth ||
 		canvas.height !== displayHeight;
 
 	if (needResize) {
-		// Make the canvas the same size
 		canvas.width = displayWidth;
 		canvas.height = displayHeight;
 	}
