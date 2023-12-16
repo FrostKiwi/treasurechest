1175637c3e156150659ce864907bfa8b509d8921
Wladislav ヴラド Artsimovich <git@frost.kiwi>
2023-12-16 17:20:55 +0900

    simpler resize logic

--- posts/GLSL-noise-and-radial-gradient/canvas_1.js
+++ posts/GLSL-noise-and-radial-gradient/canvas_1.js
@@ -30,10 +30,6 @@ var vtx = gl.getAttribLocation(shaderProgram, "vtx");
 gl.enableVertexAttribArray(vtx);
 gl.vertexAttribPointer(vtx, 2, gl.FLOAT, false, 0, 0);
 
-const canvasToDisplaySizeMap = new Map([[canvas, [-1, -1]]]);
-const resizeObserver = new ResizeObserver(onResize);
-resizeObserver.observe(canvas, { box: 'content-box' });
-
 function redraw() {
 	gl.viewport(0, 0, canvas.width, canvas.height);
 	gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
@@ -46,46 +42,15 @@ function createAndCompileShader(type, source) {
 	return shader;
 }
 
-function onResize(entries) {
-	for (const entry of entries) {
-		let width;
-		let height;
-		let dpr = window.devicePixelRatio;
-		if (entry.devicePixelContentBoxSize) {
-			width = entry.devicePixelContentBoxSize[0].inlineSize;
-			height = entry.devicePixelContentBoxSize[0].blockSize;
-			dpr = 1;
-		} else if (entry.contentBoxSize) {
-			if (entry.contentBoxSize[0]) {
-				width = entry.contentBoxSize[0].inlineSize;
-				height = entry.contentBoxSize[0].blockSize;
-			} else {
-				width = entry.contentBoxSize.inlineSize;
-				height = entry.contentBoxSize.blockSize;
-			}
-		} else {
-			width = entry.contentRect.width;
-			height = entry.contentRect.height;
-		}
-		const displayWidth = Math.round(width * dpr);
-		const displayHeight = Math.round(height * dpr);
-		canvasToDisplaySizeMap.set(entry.target, [displayWidth, displayHeight]);
-	}
-	if (resizeCanvasToDisplaySize(canvas)) {
+function onResize() {
+	const width = Math.round(canvas.clientWidth * window.devicePixelRatio);
+	const height = Math.round(canvas.clientHeight * window.devicePixelRatio);
+
+	if (canvas.width !== width || canvas.height !== height) {
+		canvas.width = width;
+		canvas.height = height;
 		redraw();
 	}
 }
-
-function resizeCanvasToDisplaySize(canvas) {
-	const [displayWidth, displayHeight] = canvasToDisplaySizeMap.get(canvas);
-
-	const needResize = canvas.width !== displayWidth ||
-		canvas.height !== displayHeight;
-
-	if (needResize) {
-		canvas.width = displayWidth;
-		canvas.height = displayHeight;
-	}
-
-	return needResize;
-}
+window.addEventListener('resize', onResize, true);
+onResize();
